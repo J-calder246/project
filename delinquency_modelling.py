99a2e7883de72656f50c14bb203d7c3d591a3a8a
Joe calder <2511607@leedstrinity.ac.uk>
2026-07-06 16:43:58 +0000

    Uploaded fannie may 2019 dataset and am undertaking further processing to prepare it for the second layer modelling that will predict delinquency

---
+++ delinquency_modelling.py
@@ -0,0 +1,12 @@
+"""
+Modelling mortgage delinquency
+-------------------------------
+Figure out:
+        - What features fit with the other dataset
+        - save the positive predictions from the first model
+        - train model on 2024Q1 dataset
+        - Predict delinquency with the 2nd model
+
+
+
+"""
